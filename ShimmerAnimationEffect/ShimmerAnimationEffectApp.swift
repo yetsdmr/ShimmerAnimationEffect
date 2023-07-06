f5392ccd04ddaa9f65b94f02133e5eab4d7c3f53
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-07-06 15:15:50 +0300

    Initial Commit

---
+++ ShimmerAnimationEffect/ShimmerAnimationEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShimmerAnimationEffectApp.swift
+//  ShimmerAnimationEffect
+//
+//  Created by Yunus Emre Taşdemir on 6.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct ShimmerAnimationEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
