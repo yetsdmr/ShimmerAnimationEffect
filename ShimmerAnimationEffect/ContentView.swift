9dc70488d55572aa40061d8b6bc0ac3f45d1fa81
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-07-07 15:40:28 +0300

    Added shimmer effect.

--- ShimmerAnimationEffect/ContentView.swift
+++ ShimmerAnimationEffect/ContentView.swift
@@ -15,13 +15,52 @@ struct ContentView: View {
                     .font(.title)
                     .fontWeight(.black)
                     // Shimmer Effect
+                    .shimmer(.init(tint: .white.opacity(0.5), highlight: .white, blur: 5))
                     .padding()
                     .background {
                         RoundedRectangle(cornerRadius: 15, style: .continuous)
                             .fill(.red.gradient)
                     }
+                
+                HStack(spacing: 15) {
+                    ForEach(["suit.heart.fill", "box.truck.badge.clock.fill",
+                             "sun.max.trianglebadge.exclamationmark.fill"], id: \.self) { sfImage in
+                        Image(systemName: sfImage)
+                            .font(.title)
+                            .fontWeight(.black)
+                            .shimmer(.init(tint: .white.opacity(0.4), highlight: .white, blur: 5))
+                            .frame(width: 40, height: 40)
+                            .padding()
+                            .background {
+                                RoundedRectangle(cornerRadius: 15, style: .continuous)
+                                    .fill(.indigo.gradient)
+                            }
+                    }
+                }
+                
+                // Another Example
+                HStack {
+                    Circle()
+                        .frame(width: 55, height: 55)
+                    VStack(alignment: .leading, spacing: 6) {
+                        RoundedRectangle(cornerRadius: 4)
+                            .frame(height: 10)
+                        
+                        RoundedRectangle(cornerRadius: 4)
+                            .frame(height: 10)
+                            .padding(.trailing, 50)
+                        
+                        RoundedRectangle(cornerRadius: 4)
+                            .frame(height: 10)
+                            .padding(.trailing, 100)
+                    }
+                }
+                .padding(15)
+                .padding(.horizontal, 30)
+                .shimmer(.init(tint: .gray.opacity(0.3), highlight: .white, blur: 5))
             }
             .navigationTitle("Shimmer Effect")
+            //.preferredColorScheme(.dark)
         }
     }
 }
